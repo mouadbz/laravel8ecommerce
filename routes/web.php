08784a81ba00a5f4917aa4f9fa957cb3d96f6207
mouadbz <57324354+mouadbz@users.noreply.github.com>
2021-06-15 21:23:24 +0100

    3cmt

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,8 @@ use App\Http\Livewire\HomeComponent ;
 use App\Http\Livewire\ShopComponent ;
 use App\Http\Livewire\CartComponent ;
 use App\Http\Livewire\CheckoutComponent ;
-
-
-
+use App\Http\Livewire\user\UserDashboardComponent ;
+use App\Http\Livewire\admin\AdminDashboardComponent ;
 
 
 Route::get('/',HomeComponent::class);
@@ -16,6 +15,19 @@ Route::get('/cart',CartComponent::Class);
 Route::get('/checkout',CheckoutComponent::class);
 
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+   // return view('dashboard');
+//})->name('dashboard');
+//For Admin
+Route::middleware(['auth:sanctum','verified','authadmin'])->group(function(){
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
+
+//For user or customer
+Route::middleware(['auth:sanctum','verified','authadmin'])->group(function(){
+
+    Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
+
+
+}); 
